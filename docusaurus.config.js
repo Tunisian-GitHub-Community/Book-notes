b1d1a7acb3f94567490f5380d3ebdedb34dbbd50
ayoub3bidi <mrayoubabidi@gmail.com>
2024-07-06 20:04:16 +0100

    fix: fix edit this page link (#8)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/',
+            'https://github.com/Tunisian-GitHub-Community/Book-notes/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
